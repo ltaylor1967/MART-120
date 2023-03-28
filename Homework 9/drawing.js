d5227023ad71d02d6af89c45facb65f7549c17a3
ltaylor1967 <123435705+ltaylor1967@users.noreply.github.com>
2023-03-27 19:37:27 -0600

    Homework 9

---
+++ Homework 9/drawing.js
@@ -0,0 +1,28 @@
+function setup()
+{
+    createCanvas(400,400)
+}
+
+function draw()
+{
+    background(10,124,130);
+    fill(10);
+    textSize(27);
+    text('My Self Portrait!', 100, 25);
+    fill(235,300);
+    circle(195,100,80);
+    fill('pink')
+    triangle(150, 280, 195, 165, 240, 280);
+    rect(170,140,50,85);
+    line(160, 150, 150, 230);
+    line(230, 150, 240, 230);
+    line(185, 285, 185, 340);
+    line(205, 285, 205, 340);
+    line(182, 123, 208, 118);
+    line(195, 97, 195, 110);
+    point(183, 89);
+    point(207, 89);
+    textSize(16);
+    fill(300);
+    text('Lucy Taylor', 295, 380);
+}
